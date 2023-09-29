fa1844a3339f63274b6e32628b9d02e795b0414f
harshil.junakiya <harshiljunakiya123@gmail.com>
2023-09-29 18:12:30 +0530

    Main

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request
+import numpy as np
+import pandas as pd
+import difflib
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+app = Flask(__name__)
+
+# Load and preprocess your movie data
+movies_data = pd.read_csv('./movies.csv')
+selected_features = ['genres', 'keywords', 'tagline', 'cast', 'director']
+for feature in selected_features:
+    movies_data[feature] = movies_data[feature].fillna('')
+combined_features = movies_data['genres'] + ' ' + movies_data['keywords'] + ' ' + movies_data['tagline'] + ' ' + movies_data['cast'] + ' ' + movies_data['director']
+vectorizer = TfidfVectorizer()
+feature_vectors = vectorizer.fit_transform(combined_features)
+similarity = cosine_similarity(feature_vectors)
+
+# Function to get movie recommendations
+def get_movie_recommendations(movie_name):
+    list_of_all_titles = movies_data['title'].tolist()
+    find_close_match = difflib.get_close_matches(movie_name, list_of_all_titles)
+    if not find_close_match:
+        return []
+    close_match = find_close_match[0]
+    index_of_the_movie = movies_data[movies_data.title == close_match]['index'].values[0]
+    similarity_score = list(enumerate(similarity[index_of_the_movie]))
+    sorted_similar_movies = sorted(similarity_score, key=lambda x: x[1], reverse=True)
+    recommended_movies = []
+    for movie in sorted_similar_movies:
+        index = movie[0]
+        title_from_index = movies_data[movies_data.index == index]['title'].values[0]
+        recommended_movies.append(title_from_index)
+    return recommended_movies[:10]  # Return the top 10 recommendations
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        user_input = request.form['movie_name']
+        recommended_movies = get_movie_recommendations(user_input)
+        return render_template('index.html', recommended_movies=recommended_movies)
+    return render_template('index.html', recommended_movies=None)
+
+if __name__ == '__main__':
+    app.run(debug=True)
